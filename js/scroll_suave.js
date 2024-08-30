c6b93a64e06d0282eb873944e9e7fce75b7779a2
SantiRock <sqp.dev@gmail.com>
2024-08-29 21:07:28 -0500

    info 4

--- js/scroll_suave.js
+++ js/scroll_suave.js
@@ -3,8 +3,10 @@ const info = document.getElementById('info_w');
 info.addEventListener('click', function (e) {
     e.preventDefault();
     const targetElement = document.getElementById('audio_player');
+    console.log(targetElement)
 
-    const offsetPosition = targetElement.offsetTop - 15;
+    const offsetPosition = targetElement.offsetTop + 45;
+    console.log(offsetPosition);
 
     window.scrollTo({
         top: offsetPosition,
